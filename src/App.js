ceedbd6563c10feb32b8504c408e9c0dbbf48df4
eliphat.f.s <shiruoxi2010@163.com>
2021-01-06 21:58:13 +0800

    Pixi infrastructure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import * as PIXI from 'pixi.js';
+/*import logo from './logo.svg';
+<img src={logo} className="App-logo" alt="logo" />*/
+// Image use example
+// Get the texture for rope.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.main = new PIXI.Application({width: 800, height: 480, backgroundColor: 0x1099bb});
+    // const app = this.main;
+  }
+
+  componentDidMount() {
+    this.domref.appendChild(this.main.view);
+  }
+
+  componentWillUnmount() {
+    this.domref.removeChild(this.main.view);
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return <div ref={ (domref) => this.domref = domref }></div>;
+  }
 }
 
 export default App;
