b4c71adce4b38a935636263e402839530bd95796
eliphat.f.s <shiruoxi2010@163.com>
2021-01-10 18:46:28 +0800

    App adaption from initials

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import './App.css';
 import React from 'react';
 import * as PIXI from 'pixi.js';
-/*import logo from './logo.svg';
-<img src={logo} className="App-logo" alt="logo" />*/
-// Image use example
-// Get the texture for rope.
+
+class PixiApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.main = new PIXI.Application({width: 800, height: 480, backgroundColor: 0x1099bb});
+    }
+
+    componentDidMount() {
+        this.domref.appendChild(this.main.view);
+    }
+
+    componentWillUnmount() {
+        this.domref.removeChild(this.main.view);
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return <div ref={ (domref) => this.domref = domref }></div>;
+    }
+}
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.main = new PIXI.Application({width: 800, height: 480, backgroundColor: 0x1099bb});
-    // const app = this.main;
-  }
-
-  componentDidMount() {
-    this.domref.appendChild(this.main.view);
-  }
-
-  componentWillUnmount() {
-    this.domref.removeChild(this.main.view);
-  }
-
-  shouldComponentUpdate() {
-    return false;
-  }
-
-  render() {
-    return <div ref={ (domref) => this.domref = domref }></div>;
-  }
+    render() {
+        return <PixiApp />;
+    }
 }
 
 export default App;
