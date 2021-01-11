d076c58dd6e114b5bd14756aecfb3b1b60c36cd2
eliphat.f.s <shiruoxi2010@163.com>
2021-01-11 18:46:09 +0800

    Micro Kernel Infrastructure Tests

---
+++ src/Tensor/MicroKernel/MicroKernelInfrastructure.test.js
@@ -0,0 +1,12 @@
+import Tensor from '../Tensor'
+import BaseMicroKernel from './BaseMicroKernel'
+import MicroKernelMultiplexer from './MicroKernelMultiplexer'
+
+test('error on direct uses', () => {
+    expect(() => new BaseMicroKernel().dispatch()).toThrowError();
+    expect(() => new BaseMicroKernel().dispatch(new Tensor([], Uint8Array))).toThrowError();
+    expect(() => new MicroKernelMultiplexer().dispatch()).toThrowError();
+    expect(() => new MicroKernelMultiplexer().invoke(new Tensor([], Uint8Array).view())).toThrowError();
+    expect(() => new MicroKernelMultiplexer().checkInputs()).toThrowError();
+    expect(() => new MicroKernelMultiplexer().computeShape()).toThrowError();
+});
