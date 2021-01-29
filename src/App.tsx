fae1b6ebccc251e0fc86e2f46c06c88f0a14e757
eliphat.f.s <shiruoxi2010@163.com>
2021-01-29 11:38:42 +0800

    Basic composition

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,23 @@
 import './App.css';
 import React from 'react';
-import DynamicImshow from './Render/DynamicImshow';
 import * as MUICore from '@material-ui/core';
 import * as MIcons from '@material-ui/icons';
+import DynamicImshowTyped from './Render/DynamicImshowTyped';
+import SpaceGrid from './FVM/SpaceGrid';
 
 function CanvasApp(props: any) {
-    let data: number[][]= new Array<number[]>(256).fill([]).map((_) => new Array<number>(256).fill(0));
-    let t: number = 0;
+    let data = new Float32Array(256 * 256);
 
-    const canvasRef: React.MutableRefObject<HTMLCanvasElement | null> = React.useRef(null);
+    const canvasRef = React.useRef<HTMLCanvasElement>(null);
     React.useEffect(() => {
         const ctx = canvasRef.current?.getContext("2d")!;
-        const renderer = new DynamicImshow(ctx, data);
+        const renderer = new DynamicImshowTyped(ctx, data, 256, 256);
+        let grid = new SpaceGrid(256, 4, 340);
         tick();
         
         function tick() {
-            for (let i = 0; i < 256; i++)
-                for (let j = 0; j < 256; j++) {
-                    let d = Math.sqrt((i - 20) * (i - 20) + (j - 20) * (j - 20));
-                    let d2 = Math.sqrt((i - 40) * (i - 40) + (j - 20) * (j - 20));
-                    let tx = t - d * 2;
-                    let tx2 = t - d2 * 2;
-                    data[i][j] = tx < 0 ? 0 : Math.sin(tx * 0.24) / Math.sqrt(d + 1)
-                    data[i][j] += tx2 < 0 ? 0 : Math.sin(tx2 * 0.24) / Math.sqrt(d2 + 1);  // (t < 10) ? -100 : (20 * Math.log10(1 / Math.sqrt(d + 1)));
-                }
-            t++;
+            grid.evolve(0.4 * 4 / 256 / 340);
+            data.set(grid.u0.toTensor().raw);
             renderer.render(ctx);
             requestAnimationFrame(tick);
         }
