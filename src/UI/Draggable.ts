5d67076afb82730b8daa3a8fa2ed09d61efe6cbd
eliphat.f.s <shiruoxi2010@163.com>
2021-01-31 20:49:42 +0800

    Draggable UI script

---
+++ src/UI/Draggable.ts
@@ -0,0 +1,22 @@
+export function registerDraggable(element: HTMLElement) {
+    let dragging = false;
+    let pos: [number, number] = [0, 0];
+    element.addEventListener("mousedown", (e) => {
+        dragging = true;
+        pos = [e.clientX, e.clientY];
+    });
+    document.addEventListener("mouseup", (e) => {
+        dragging = false;
+    });
+    element.addEventListener("mousemove", (e) => {
+        if (!dragging) return;
+        e.preventDefault();
+        element.style.top = (element.offsetTop - (pos[1] - e.clientY)) + "px";
+        element.style.left = (element.offsetLeft - (pos[0] - e.clientX)) + "px";
+        pos = [e.clientX, e.clientY];
+    });
+};
+
+const elements = document.getElementsByClassName("draggable");
+for (let i = 0; i < elements.length; ++i)
+    registerDraggable(elements[i] as HTMLElement);
