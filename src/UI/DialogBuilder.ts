9758fc64bf59ad17f317e6e122b2ecf911537b39
eliphat.f.s <shiruoxi2010@163.com>
2021-03-03 18:04:35 +0800

    More functionalities for Dialog Builder

--- src/UI/DialogBuilder.ts
+++ src/UI/DialogBuilder.ts
@@ -1,6 +1,27 @@
+import { TokenFlags } from "typescript";
+
+export class DialogBuilderDataDelegate {
+    dialog: HTMLElement
+    onCommit: (() => void)[]
+    constructor(dialog: HTMLElement, onCommit: (() => void)[]) {
+        this.dialog = dialog;
+        this.onCommit = onCommit;
+    }
+
+    close() {
+        this.dialog.remove();
+    }
+
+    commitFields() {
+        for (let act of this.onCommit) act();
+        return this;
+    }
+}
+
 class DialogBuilder {
     dialog: HTMLElement
     head: HTMLElement
+    onCommit: (() => void)[]
     constructor() {
         this.dialog = document.createElement("div");
         this.dialog.style.position = "fixed";
@@ -8,23 +29,88 @@ class DialogBuilder {
         this.dialog.style.transform = "translate(-50%, -50%)";
         this.dialog.style.backgroundColor = "#ffffff";
         this.dialog.style.boxShadow = "0.0em 0.3em 0.5em rgba(0, 0, 0, 0.2)";
-        this.dialog.style.padding = "0.3em";
+        this.dialog.style.padding = "0.7em";
         this.head = this.dialog;
+        this.onCommit = [];
     }
 
-    row() {
+    rowDiv() {
         let r = document.createElement("div");
         this.dialog.appendChild(r);
         this.head = r;
         return this;
     }
 
-    button(text: string, action: (dialog: HTMLElement) => void) {
+    rowForm(title?: string) {
+        let f = document.createElement("form");
+        let s = document.createElement("fieldset");
+        f.appendChild(s);
+        f.className = "pure-form";
+        if (title) s.appendChild(document.createElement("legend")).innerHTML = title;
+        this.head = s;
+        this.dialog.appendChild(f);
+        return this;
+    }
+
+    splitter(opacity: number = 0.2) {
+        let hr = document.createElement("hr");
+        hr.style.opacity = opacity.toString();
+        this.dialog.appendChild(hr);
+        this.head = this.dialog;
+        return this;
+    }
+
+    button(text: string, action: (data: DialogBuilderDataDelegate) => void, options: {
+        size?: number, primary?: boolean, color?: string, textColor?: string
+    } = {}) {
+        let {size, primary, textColor, color} = options;
         let btn = document.createElement("button");
         btn.innerHTML = text;
         btn.className = "pure-button";
-        btn.onclick = () => action(this.dialog);
+        btn.style.fontSize = ((size ?? 0.8) * 100).toString() + "%";
+        if (primary) btn.classList.add("pure-button-primary");
+        if (color) btn.style.backgroundColor = color;
+        if (textColor) btn.style.color = textColor;
+        btn.onclick = () => action(new DialogBuilderDataDelegate(this.dialog, this.onCommit));
         this.head.appendChild(btn);
+        return this;
+    }
+
+    space() {
+        return this.span(" ");
+    }
+
+    span(text: string) {
+        let spn = document.createElement("span");
+        spn.innerHTML = text;
+        this.head.appendChild(spn);
+        return this;
+    }
+
+    textInputField(options: {
+        onCommit?: (contents: string) => void, onValidate?: (contents: string) => string | null, initial?: string, placeholder?: string, id?: string, label?: string
+    })
+    {
+        let {onCommit, onValidate, initial, placeholder, id, label} = options;
+        let tif = document.createElement("input");
+        tif.type = "text";
+        if (id) tif.id = id;
+        if (placeholder) tif.placeholder = placeholder;
+        if (initial) tif.value = initial;
+        if (label) {
+            let lab = document.createElement("label");
+            if (!id) id = "dialog-builder-generated-" + Math.random().toString();
+            lab.htmlFor = id;
+            this.head.appendChild(lab);
+        }
+        this.onCommit.push(() => { if(onCommit) onCommit(tif.value); });
+        tif.onblur = () => {
+            if (onValidate) {
+                const val = onValidate(tif.value);
+                if (val) tif.value = val;
+            }
+        }
+        this.head.appendChild(tif);
         return this;
     }
 
